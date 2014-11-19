0e23e5971e059f4645e45ad9c23ec887a6adab9c
JHagle <rob.hagle@gmail.com>
2014-11-18 20:27:37 -0600

    Most Popular implemented

---
+++ controllers/themes.py
@@ -0,0 +1,64 @@
+
+
+def index():
+
+    #session.flash = "Select a theme for your LMS"
+    
+    rows = db(db.theme.name).select()
+
+    return dict(rows = rows)
+
+
+def theme_picked():
+
+    '''
+    Build the url to the css style page from arg, 
+    and assign it to session.current_theme
+    '''
+    static_subfolder = request.args(0)
+    css_filename = request.args(1)
+    
+    css_pathname = static_subfolder + '/'  + css_filename
+    session.current_theme = URL('static', css_pathname)
+
+	# Get the rows for this css, can just reuse URL as identifier
+    picked = db(db.theme.URL == css_pathname).select()
+    
+    # Get the first use_count for the first row (there should only be one...)
+    uses = int(picked[0].use_count) + 1
+
+	# This update syntax works
+    db(db.theme.URL == css_pathname).update(use_count=uses)
+	
+	# See table printed out, use_counts will increment. 
+    
+
+    redirect(URL('themes', 'index'))    
+
+    return dict()
+
+def display_popular():
+    for row in db(db.theme).select(db.theme.name, orderby=db.theme.use_count*-1, limitby=(0, 3)):
+        row.name
+
+
+def preview():
+    
+    subfolder = request.args(0)
+    filename = request.args(1)
+
+    session.preview_theme = subfolder + '/' + filename
+    return dict()
+
+'''
+user can set a course's theme here 
+
+TODO: modify smartgrid to only show certain fields, 
+join to themes table to show theme names
+'''
+@auth.requires(auth.user and auth.user.is_administrator)
+def course_themes():
+    grid=SQLFORM.smartgrid(db.course, linked_tables=['theme'])
+
+    return dict(grid=grid)
+
---
+++ models/menu_cal.py
@@ -0,0 +1,10 @@
+## Create the calendar menu.
+if auth.is_logged_in():
+    response.menu += [
+        (T('Calendar'), False, URL('calendar', 'index'), [
+                (T('Create Event'), False, URL('calendar', 'create'), []),
+                (T('My Calendar'), False, URL('calendar', 'index'), []),
+                (T('Course Calendar'), False, URL('calendar', 'course_calendar'), []),
+                (T('Delete Event'), False, URL('calendar', 'delete'), []),
+        ])
+    ]
