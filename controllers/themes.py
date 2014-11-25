a3b237aa5e6b96a2c6f2b0b59a432666e193b3af
drkaff <drkaff@gmail.com>
2014-11-25 11:31:37 -0600

    Update themes.py

--- controllers/themes.py
+++ controllers/themes.py
@@ -1,10 +1,15 @@
-
+import os,json,sys
+from pprint import pprint
+import shutil
+from subprocess import call
+import re
+import time
 
 def index():
+
     #session.flash = "Select a theme for your LMS"    
     rows = db(db.theme.name).select()
-    images=db(db.image.name).select()
-    return dict(rows = rows, images=images)
+    return dict(rows = rows)
 
 
 def theme_picked():
@@ -14,54 +19,23 @@ def theme_picked():
     """
     static_subfolder = request.args(0)
     css_filename = request.args(1)
-
+    
     session.prev_theme = session.current_theme
     
     css_pathname = static_subfolder + '/'  + css_filename
-    session.css_pathname = css_pathname
     session.current_theme = URL('static', css_pathname)
     print session.current_theme
-
-	# Get the rows for this css, can just reuse URL as identifier
+    
+    # Get the rows for this css, can just reuse URL as identifier
     picked = db(db.theme.URL == css_pathname).select()
     
-    # Get the first use_count for the first row (there should only be one...)
-    uses = int(picked[0].use_count) + 1
 
-	# This update syntax works
-    db(db.theme.URL == css_pathname).update(use_count=uses)
-
-    #redirect(URL('themes', 'index'))    
-    redirect(URL('themes', 'countdown'))
+    redirect(URL('themes', 'index'))    
 
     return dict()
 
-def countdown():
-    return dict()
-
-'''
-If user rejects theme, they get sent here
-'''
-def themeBack():
-
-    # delete 1 from theme count for the reject theme
-    rejected_theme = db(db.theme.URL == session.css_pathname).select()
-    uses = int(rejected_theme[0].use_count) - 1
-    db(db.theme.URL == session.css_pathname).update(use_count=uses)
-
-    #reset theme back to previous
-    session.current_theme = session.prev_theme
-    session.prev_theme = ''
-    redirect(URL('themes', 'index'))
-    return dict()
-
-def display_popular():
-    for row in db(db.theme).select(db.theme.name, orderby=db.theme.use_count*-1, limitby=(0, 3)):
-        row.name, row.use_count
-
-
-def preview():    
-   
+def preview():
+    
     if request.args(0) and request.args(1):
         subfolder = request.args(0)
         filename = request.args(1)
@@ -71,7 +45,6 @@ def preview():
         session.preview_theme = 'images/default.jpg'
 
     return dict()
-
 '''
 user can set a course's theme here 
 
@@ -84,3 +57,64 @@ def course_themes():
 
     return dict(grid=grid)
 
+def theme_create():
+
+    fp = os.path.join(request.folder,'private','bootstrap.default.json')   
+
+    json_data=open(fp)
+    #theme default data
+    tdd = json.load(json_data)
+    json_data.close()
+    calll = '';
+    errors = []
+    error = '';
+    name = ''; filename = ''
+    
+    if request.post_vars:
+        #create theme
+        try:
+            if request.post_vars['name'] == '':
+                errors.append('You need set name')
+                raise Exception()
+            if request.post_vars['url'] == '':
+                errors.append('You need set filename')
+                raise Exception()
+
+            name = request.post_vars['name']
+            filename = request.post_vars['url']
+            fp_less = os.path.join(request.folder,'private','less')  
+            fp_copy = os.path.join(request.folder,'private',response.session_id + '_les_' + str(time.time())) 
+            fp_css = os.path.join(request.folder,'static','css') 
+            shutil.copytree(fp_less, fp_copy)
+            var_data = []
+            tdd.update(request.post_vars)
+            
+            for key,value in tdd.iteritems():
+                if re.match('/^@/', key):
+                  var_data.append(key+': ' + value + ';')
+               
+            f = open(fp_copy + '/variables.less', 'w')
+            f.write("\n".join(var_data))
+            f.write('This is a test\n')
+            f.close()
+
+            
+            calll =  'lesscpy ' + fp_copy + '/bootstrap.less' 
+            calll += fp_css + '/' + filename + '.css' 
+            os.system(calll)
+            #calll(['lesscpy ' + fp_copy + '/bootstrap.less  > ' + fp_css + '/' + response.session_id + '_bootstrap.css'])
+            shutil.rmtree(fp_copy)
+
+            db.theme.insert(name=name, 
+                URL = 'css/' + filename + '.css',  image_URL = 'images/default.jpg')
+            
+
+        except Exception, e:
+            error = e
+            errors.append('some happend ')
+
+        if len(errors) == 0 :
+            redirect(URL('themes', 'index'))   
+
+
+    return dict(tdd = tdd, calll = calll, errors = errors, error= error, name= name, filename = filename)
